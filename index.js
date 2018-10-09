03a8a653f4414c9875a51c2d0071fa5cf8940a38
alexandraaab <43570109+alexandraaab@users.noreply.github.com>
2018-10-09 16:14:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,88 +1,60 @@
-//--------------------------------------------------
+var vectorSource = new ol.source.Vector({});
 
- var map = new ol.Map({
-    layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
+var layerRuta = new ol.layer.Vector({
+    source: vectorSource,
+    title: "Pinpoints",
+    style: new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            color: 'white',
+            width: 1
+        }),
+        fill: new ol.style.Fill({
+            color: 'rgba(255,0,0,0.1)'
+        })
+    })
+});
+
+var drawSource = new ol.source.Vector();
+    
+var styleFunction = function(feature) {
+    var geometry = feature.getGeometry();
+    var coordinates = geometry.getCoordinates();
+    var styles = [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+            color: '#0003db',
+            width: 3
+            })
+        })
+    ];
+    
+    var form = document.getElementById("sendrun");
+    form.elements['distance'].value=geometry.getLength()
+    form.elements['coordinates'].value=coordinates
+       
+    return styles;
+};
+    
+var vector = new ol.layer.Vector({
+    source: drawSource,
+    style: styleFunction
+});
+
+var map = new ol.Map({
+    layers: [new ol.layer.Tile({ source: new ol.source.OSM() }),layerRuta, vector],
     target: document.getElementById('map'),
     view: new ol.View({
-        center: [2015491.5618235283, 8289494.729588233],
-        zoom: 7
+        projection: 'EPSG:3857',
+        center: [0, 0],
+        zoom: 3
     })
-  });
+});
 
-loadPins();
+//------------------------------
 
-//-------------Functions--------------------------------
-
-function drawRoute(evt){
-    var source = new ol.source.Vector();
-
-    var styleFunction = function(feature) {
-      var geometry = feature.getGeometry();
-       var styles = [
-         // linestring
-         new ol.style.Style({
-           stroke: new ol.style.Stroke({
-             color: '#3138ff',
-             width: 3
-           })
-         })
-       ];
-    
-       geometry.forEachSegment(function(start, end) {
-         var dx = end[0] - start[0];
-         var dy = end[1] - start[1];
-         styles.push(new ol.style.Style({
-           geometry: new ol.geom.Point(end),
-         }));
-       });
-    
-       return styles;
-     };
-    
-     var vector = new ol.layer.Vector({
-       source: source,
-       style: styleFunction
-     });
-
-     map.addLayer(vector);
-    
-
-     map.addInteraction(new ol.interaction.Draw({
-        source: source,
-        type: 'LineString'
-      }));
-
-
-      var modify = new ol.interaction.Modify({source: source});     //Gör så att den ritade linjen går att ändra i efterhand 
-      map.addInteraction(modify);
-
-    //popup (dyker upp vid dubbelklick)
-      var popup = new ol.Overlay({
-        element: document.getElementById('popup')
-    });
-    
-    map.addOverlay(popup);
-    
-    map.on('dblclick', function(evt) {
-        var coordinate = evt.coordinate;
-        popup.setPosition(coordinate);
-        document.getElementById("lon").value =coordinate[0];
-        document.getElementById("lat").value =coordinate[1];
-        var marker = new ol.Overlay({
-            position: coordinate,
-            positioning: 'center-center',
-            element: document.getElementById('marker'),
-            stopEvent: false
-        });
-      map.addOverlay(marker);
-    });
-}
-
-//--------------Laddar gamla pins från databasen, inte några rutter----------------
-
-function loadPins(evt){
+function loadroads(evt){
     var req = $.ajax ({
-        url: "http://localhost:3000/loadpins",
+        url: "http://localhost:3000/loadroads",
         type: "GET",
     });
     
@@ -90,46 +62,23 @@ function loadPins(evt){
         console.log(JSON.stringify(resp_json));
         var listaFeat=jsonAnswerDataToListElements(resp_json);
    
-        var pointdata= {
+        var linjedata= {
             "type": "FeatureCollection",
             "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4326" }},
 
             "features": listaFeat
         };
+
+        vectorSource.addFeatures( (new ol.format.GeoJSON()).readFeatures(linjedata));
         
-        console.log(JSON.stringify(pointdata));
-        var vectorSource = new ol.source.Vector({
-            features: (new ol.format.GeoJSON()).readFeatures(pointdata)
-        });
-
+        console.log(JSON.stringify(linjedata));
         console.log(vectorSource)
-        var white = [255, 255, 255, 1];
-        var blue = [0, 153, 255, 1];
-        var red = [255,0,0,1];
-
-        var layerRuta = new ol.layer.Vector({
-            source: vectorSource,
-            title: "Pinpoints",
-            style: new ol.style.Style({
-                image: new ol.style.Circle({
-                    radius: 3 * 2,
-                    fill: new ol.style.Fill({
-                      color: blue
-                    }),
-                    stroke: new ol.style.Stroke({
-                      color: white,
-                      width: 3 / 2
-                    })
-                }),
-            })
-        });
         console.log(layerRuta)
-        map.addLayer(layerRuta);
-          
+        
     });
 }
 
-//-----------------------------------------
+//----------------------------------------------------
 
 function jsonAnswerDataToListElements(json_answer){
     data=json_answer;
@@ -143,7 +92,185 @@ function jsonAnswerDataToListElements(json_answer){
     return r;
 }
 
-//--------------------------------------------------
+//Find my position
+function geoFindMe() {
+    var output = document.getElementById("out");
+  
+    if (!navigator.geolocation){
+      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+      return;
+    }
+  
+    function success(position) {
+        var latitude  = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+        var coords = ol.proj.fromLonLat([longitude,latitude]);
+        map.getView().animate({center: coords, zoom: 13});
+    }
+  
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    }
+  
+    output.innerHTML = "<p>Locating…</p>";
+  
+    navigator.geolocation.getCurrentPosition(success, error);
+}
 
+//------------------------------------
+
+function postRun() {
+    var form = document.getElementById("sendrun");
+    data=JSON.stringify ({
+        distance: form.elements[0].value,
+        description: form.elements[1].value,
+        routename: form.elements[2].value,
+        cityarea: form.elements[3].value,
+        terrain: form.elements[4].value,
+        private: form.elements[5].value,
+        coordinates: form.elements[6].value
+    })
+    alert("all good")
+	
+	var req = $.ajax ({
+        url: "http://localhost:3000/postrun",
+        type: "POST",
+        cache: false,
+        contentType: "application/json",
+        data: data
+    });
+    
+    req.done(function(resp_json) {
+        alert(resp_json)
+    });
+    resetForm()
+    drawRoute(false)
+}
+
+//----------------------------------------
+
+function resetForm(){
+    document.getElementById("sendrun").reset();  
+    drawSource.clear();  
+}
+
+//------------------------------------------------
+
+function drawRoute(evt){
+    
+    map.addInteraction(new ol.interaction.Draw({
+        source: drawSource,
+        type: 'LineString',
+        style: new ol.style.Style({
+            stroke: new ol.style.Stroke({
+              color: '#0003db',
+              lineDash: [10, 10],
+              width: 2
+            }),
+            image: new ol.style.Circle({
+                radius: 5,
+                stroke: new ol.style.Stroke({
+                  color: 'rgba(0, 0, 0, 0.7)'
+                })
+            })
+        })
+    }));
+
+    var modify = new ol.interaction.Modify({source: drawSource});     //Gör så att den ritade linjen går att ändra i efterhand 
+    map.addInteraction(modify);
+    document.getElementById("draw").disabled = true;
+}
+//-------------------------------
+function snapRoute(evt){
+
+    //----------------Kod som ej funkar--
+
+    // drawingManager.addListener('polylinecomplete', function(poly) {
+    //     var path = poly.getPath();
+    //     polylines.push(poly);
+    //     placeIdArray = [];
+    //     runSnapToRoad(path);
+    //   });
+
+    //   function runSnapToRoad(path) {
+    //     var pathValues = [];
+    //     for (var i = 0; i < path.getLength(); i++) {
+    //       pathValues.push(path.getAt(i).toUrlValue());
+    //     }
+      
+    //     $.get('https://roads.googleapis.com/v1/snapToRoads', {
+    //       interpolate: true,
+    //       key: apiKey,
+    //       path: pathValues.join('|')
+    //     }, function(data) {
+    //       processSnapToRoadResponse(data);
+    //       drawSnappedPolyline();
+    //       getAndDrawSpeedLimits();
+    //     });
+    //   }
+
+    //   function processSnapToRoadResponse(data) {
+    //     snappedCoordinates = [];
+    //     placeIdArray = [];
+    //     for (var i = 0; i < data.snappedPoints.length; i++) {
+    //       var latlng = new google.maps.LatLng(
+    //           data.snappedPoints[i].location.latitude,
+    //           data.snappedPoints[i].location.longitude);
+    //       snappedCoordinates.push(latlng);
+    //       placeIdArray.push(data.snappedPoints[i].placeId);
+    //     }
+    //   }
+      
+    //   // Draws the snapped polyline (after processing snap-to-road response).
+    //   function drawSnappedPolyline() {
+    //     var snappedPolyline = new google.maps.Polyline({
+    //       path: snappedCoordinates,
+    //       strokeColor: 'black',
+    //       strokeWeight: 3
+    //     });
+      
+    //     snappedPolyline.setMap(map);
+    //     polylines.push(snappedPolyline);
+    //   }
+
+    /////////
+
+    function findNearestMarker(coords) {
+         
+         var source = layerRuta.getSource();
+         var Allfeatures = source.getFeatures();
+
+        var feature = layerRuta.getSource().getClosestFeatureToCoordinate(coords);
+
+        // get all objects added to the map
+
+        feature.setStyle(new ol.style.Style ({
+            fill: new ol.style.Fill({
+                color: '#db5300'
+            }),
+            stroke: new ol.style.Stroke({
+                color: '#db5300',
+                width: 2
+            })
+        })
+        );
+    }
+      
+    map.on('singleclick', function(evt) {
+        var coords = (evt.coordinate);
+        findNearestMarker(coords);
+    },  false);
+
+    document.getElementById("snap").disabled = true;
+}
+
+//-------------------------------
+
+geoFindMe()
+loadroads();
+document.getElementById("postrun").addEventListener("click", postRun);
+document.getElementById("resetform").addEventListener("click", resetForm);
 document.getElementById("draw").addEventListener("click", drawRoute);
+document.getElementById("snap").addEventListener("click", snapRoute);
 
--- server.js
+++ server.js
@@ -1,19 +1,69 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
 
-app.use(bodyParser.json()); //for parsing post data that has json format
+// app.use(bodyParser.json()); //for parsing post data that has json format
 
-app.use ( function (req , res , next){
-    res.setHeader ('Access-Control-Allow-Origin', '*');
-    res.setHeader ('Access-Control-Allow-Methods', 'GET ,POST ,PUT , DELETE , OPTIONS');
-    res.setHeader ('Access-Control-Allow-Headers', 'Content-Type');
-    next ();
+// app.use ( function (req , res , next){
+//     res.setHeader ('Access-Control-Allow-Origin', '*');
+//     res.setHeader ('Access-Control-Allow-Methods', 'GET ,POST ,PUT , DELETE , OPTIONS');
+//     res.setHeader ('Access-Control-Allow-Headers', 'Content-Type');
+//     next ();
+// });
+
+// const{Pool} = require('pg');
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'postgres',
+//     password: '1234',
+//     port: 5432,
+// });
+
+
+// //Get the data from the database 
+// app.get('/loadpins', (req ,res) => {
+//     pool.query ('select  ST_AsGeoJSON(geom) from public.coords', (err,dbResponse) => {
+//         if (err) console.log (err);
+//         console.log(dbResponse.rows);
+//         // here dbResponse is available , your data processing logic goes here
+//         res.setHeader('Access-Control-Allow-Origin','*');
+//         res.send(dbResponse.rows);
+//     });
+// });
+
+
+
+// app.post ('/pin', (req , res) => {
+//     console.log (req.body);
+//     // data you send from your application is available on
+//     // req.body object , your data processing logic goes here
+//    // pool.query (`insert into public.coords (geom) values (ST_GeomFromText('${req.body.x} ${req.body.y})',3857))`, (err, dbResponse) => {
+//     pool.query (`insert into public.coords (geom) values (ST_GeomFromText('POINT(${req.body.lon} ${req.body.lat})',3857))`, (err, dbResponse) => {
+//         if (err) console.log(err);
+//         res.send("Position added!");
+//     });
+// });
+
+// app.listen (3000 , () => console.log ('Example app listening on port3000!') );
+
+const express = require ('express');
+const app = express ();
+const bodyParser = require ('body-parser');
+
+app.use(bodyParser.json()); // for parsing post data that has json format
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    next();
 });
 
-const{Pool} = require('pg');
+const { Pool } = require ('pg');
 
-const pool = new Pool({
+const pool = new Pool ({
     user: 'postgres',
     host: 'localhost',
     database: 'postgres',
@@ -21,10 +71,8 @@ const pool = new Pool({
     port: 5432,
 });
 
-
-//Get the data from the database 
-app.get('/loadpins', (req ,res) => {
-    pool.query ('select  ST_AsGeoJSON(geom) from public.coords', (err,dbResponse) => {
+app.get('/loadroads', (req ,res) => {
+    pool.query ('select ST_AsGeoJSON(geom) from d.roads', (err,dbResponse) => {
         if (err) console.log (err);
         console.log(dbResponse.rows);
         // here dbResponse is available , your data processing logic goes here
@@ -33,18 +81,24 @@ app.get('/loadpins', (req ,res) => {
     });
 });
 
-
-
-app.post ('/pin', (req , res) => {
+app.post ('/myposition', (req , res) => {
     console.log (req.body);
     // data you send from your application is available on
     // req.body object , your data processing logic goes here
-   // pool.query (`insert into public.coords (geom) values (ST_GeomFromText('${req.body.x} ${req.body.y})',3857))`, (err, dbResponse) => {
-    pool.query (`insert into public.coords (geom) values (ST_GeomFromText('POINT(${req.body.lon} ${req.body.lat})',3857))`, (err, dbResponse) => {
+    pool.query (`insert into lab3.currentpos (geom) values (ST_GeomFromText('POINT(${req.body.x} ${req.body.y})',3857))`, (err, dbResponse) => {
+        if (err) console.log(err);
+        res.send("Position added!");
+    });
+});
+
+app.post ('/postrun', (req , res) => {
+    console.log (req.body);
+    // data you send from your application is available on
+    // req.body object , your data processing logic goes here
+    pool.query (`insert into d.track (user_gid, distance, description, route_name, cityarea, terrain, private) values ('1', '${req.body.distance}','${req.body.description}','${req.body.routename}','${req.body.cityarea}','${req.body.terrain}',${req.body.private})`, (err, dbResponse) => {
         if (err) console.log(err);
         res.send("Position added!");
     });
 });
 
 app.listen (3000 , () => console.log ('Example app listening on port3000!') );
-
